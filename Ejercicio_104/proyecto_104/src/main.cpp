defc9549f4126fd162bb4e903b776acfd38692c4
Daniel Sepulveda <daniel.sepulvedam@cun.edu.co>
2023-10-21 12:29:40 -0500

    hola

--- Ejercicio_104/proyecto_104/src/main.cpp
+++ Ejercicio_104/proyecto_104/src/main.cpp
@@ -1,18 +1,47 @@
 #include <Arduino.h>
+#include <DataCapture.h>
 
-// put function declarations here:
-int myFunction(int, int);
+struct Nodo {
+  int dato;
+  Nodo* siguiente;
+};
+
+Nodo* lista = NULL;
+
+void insertarLista(Nodo*& lista, int n) {
+  Nodo* nuevo_nodo = new Nodo();
+  nuevo_nodo->dato = n;
+  Nodo* aux1 = lista;
+  Nodo* aux2 = NULL;
+
+  while (aux1 != NULL && aux1->dato < n) {
+    aux2 = aux1;
+    aux1 = aux1->siguiente;
+  }
+
+  if (lista == aux1) {
+    lista = nuevo_nodo;
+  } else {
+    aux2->siguiente = nuevo_nodo;
+  }
+
+  nuevo_nodo->siguiente = aux1;
+  Serial.begin(9600); 
+  Serial.print("\tElemento ");
+  Serial.print(n);
+  Serial.println(" Insertado en la lista correctamente");
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  int dato;
+  Serial.begin(9600); 
+  Serial.print("Digite un número: ");
+  while (!Serial.available()) {
+  }
+  dato = Serial.parseInt();
+
+  insertarLista(lista, dato);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
